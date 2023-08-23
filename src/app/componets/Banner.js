14fc291a590e7d33bc81c53a61c7bd53adf74348
Erenakhan <mustafaakhan794@gmail.com>
2023-08-23 08:11:56 +0300

    image optimization

--- src/app/componets/Banner.js
+++ src/app/componets/Banner.js
@@ -10,7 +10,7 @@ export default function Banner() {
         alt="banner"
         fill={true}
         cover={true}
-        className='w-full h-full'
+        className='w-full h-full object-cover inset-0'
       />
     <div className="container broder-2  text-white absolute z-20 w-1/2 text-center left-0 top-0 h-full text-lg  md:text-3xl  xl:text-5xl flex justify-center items-center
     ">Sağlığınız İçin <br/>Aqua Natural&apos;i seçin</div>
