093c427f65950cb592c33cee0929e141154874cf
Erenakhan <100142188+Erenakhan@users.noreply.github.com>
2024-01-25 19:14:37 +0300

    Update Analytics.js

--- src/app/componets/Analytics.js
+++ src/app/componets/Analytics.js
@@ -15,7 +15,7 @@ export default function Analytics() {
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-B0Y3KDWCFD');
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
       `}
     </Script>
     </>
