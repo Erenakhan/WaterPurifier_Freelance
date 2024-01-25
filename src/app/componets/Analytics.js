17018710ca6d5fc6288d88c352154aaeac5e9695
Erenakhan <100142188+Erenakhan@users.noreply.github.com>
2024-01-25 19:07:50 +0300

    Update Analytics.js

--- src/app/componets/Analytics.js
+++ src/app/componets/Analytics.js
@@ -1,1 +1,24 @@
+// pages/analytics.js
+
+import Script from 'next/script';
+
+export default function Analytics() {
+  return (
+    <>
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-B0Y3KDWCFD`}
+    />
+    <Script strategy="lazyOnload">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-B0Y3KDWCFD');
+      `}
+    </Script>
+    </>
+  );
+}
 
