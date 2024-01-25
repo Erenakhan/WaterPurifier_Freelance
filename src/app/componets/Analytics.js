62f7ee3d1f127827cd5c3fa402ab1ece47080791
Erenakhan <100142188+Erenakhan@users.noreply.github.com>
2024-01-25 18:25:23 +0300

    Create Analytics.js

---
+++ src/app/componets/Analytics.js
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+export default function Analytics() {
+  return (
+   <>
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-B0Y3KDWCFD`}
+    />
+    <Script strategy="lazyOnload">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-B0Y3KDWCFD');
+      `}
+    </Script>
+    </>
+  );
+}
